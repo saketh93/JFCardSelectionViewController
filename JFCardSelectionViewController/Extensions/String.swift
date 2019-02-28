8e22615618340243273b905f03a8b9b5fae264f4
Saketh <Saketh.Manemala@cognizant.com>
2019-02-28 11:51:30 +0530

    fix from previous commit

---
+++ JFCardSelectionViewController/Extensions/String.swift
@@ -0,0 +1,65 @@
+/*
+* String
+*
+* Created by Jeremy Fox on 3/1/16.
+* Copyright (c) 2016 Jeremy Fox. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+public extension String {
+    public func contains(_ string: String) -> Bool {
+        return range(of: string) != nil
+    }
+    
+    subscript(i: Int) -> Character {
+        return self[self.characters.index(self.startIndex, offsetBy: i)]
+    }
+    
+    subscript(i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript(r: Range<Int>) -> String {
+        let start = characters.index(startIndex, offsetBy: r.lowerBound)
+        let end = characters.index(start, offsetBy: r.upperBound - r.lowerBound)
+        return self[start..<end]
+    }
+}
+
+public extension Optional {
+    public func contains(_ string: String) -> Bool {
+        switch self {
+        case .none:
+            return false
+        case let .some(value):
+            if value is Int {
+                return "\((value as! Int))".range(of: string) != nil
+            } else if value is String {
+                return (value as! String).range(of: string) != nil
+            }
+        }
+        
+        return false
+    }
+    
+    public func contains(_ int: Int) -> Bool {
+        return contains("\(int)")
+    }
+}
---
+++ JFCardSelectionViewController/Extensions/UIView.swift
@@ -0,0 +1,70 @@
+/*
+* UIView
+*
+* Created by Jeremy Fox on 3/1/16.
+* Copyright (c) 2016 Jeremy Fox. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+import UIKit
+
+public extension UIView {
+    
+    public func makeRound() {
+        self.contentMode = .scaleAspectFill;
+        self.clipsToBounds = true;
+        var f = self.frame;
+        let w = f.width;
+        let h = f.height;
+        var corner = w;
+        if (h > w) { // Portrait Orientation
+            f.size.height = w;
+        } else if (w > h) { // Landscape Orientation
+            f.size.width = h;
+            corner = h;
+        }
+        self.frame = f;
+        self.layer.cornerRadius = (corner / 2);
+    }
+    
+    public func makeRoundWithBorder(width: CGFloat, color: UIColor) {
+        makeRound()
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+    }
+    
+    public class func loadFromNibNamed(_ nibNamed: String, bundle : Bundle? = nil) -> UIView? {
+        return UINib(nibName: nibNamed, bundle: bundle).instantiate(withOwner: nil, options: nil).first as? UIView
+    }
+    
+}
+
+public protocol UIViewLoading {}
+extension UIView : UIViewLoading {}
+public extension UIViewLoading where Self : UIView {
+    
+    public static func loadFromNib() -> Self {
+        let nibName = "\(self)".characters.split{$0 == "."}.map(String.init).last!
+        let bundle = Bundle(for: Self.self)
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! Self
+    }
+    
+}
